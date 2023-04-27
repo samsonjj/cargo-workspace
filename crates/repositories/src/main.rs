4ee0d4a2e875e172cd2b05b56ad5c5bbcfbba162
Jonathan Samson <jonathansamsonvcu@gmail.com>
2023-04-27 00:13:18 -0400

    saving

--- crates/repositories/src/main.rs
+++ crates/repositories/src/main.rs
@@ -1,15 +1,15 @@
-use repositories::builder::{DatabaseObjectBuilder, DbObject, Field};
+use repositories::builder::{DatabaseObjectBuilder, Field};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let client = repositories::get_ready_client().await?;
+    // let client = repositories::get_ready_client().await?;
 
-    let create_query = DatabaseObjectBuilder::new("foo_table")
-        .add(Field::int("hello", "description test"))
-        .add(Field::int("goodbye", "description test"))
-        .build();
+    // let create_query = DatabaseObjectBuilder::new("foo_table")
+    //     .add(Field::int("hello", "description test"))
+    //     .add(Field::int("goodbye", "description test"))
+    //     .build();
 
-    client.execute(&create_query[..], &[]).await?;
+    // client.execute(&create_query[..], &[]).await?;
 
     Ok(())
 }
@@ -18,8 +18,8 @@ struct PageView {
     name: String,
 }
 
-impl DbObject for PageView {
-    fn get_fields(builder: &mut DatabaseObjectBuilder) {
-        builder.add(Field::int("Hello", "description test"));
-    }
-}
+// impl DbObject for PageView {
+//     fn get_fields(builder: &mut DatabaseObjectBuilder) {
+//         builder.add(Field::int("Hello", "description test"));
+//     }
+// }
